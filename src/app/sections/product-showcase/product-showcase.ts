778adede6a11ddfee2a787da2434d1dd050c0315
Umeozulu Anthony <tonyumeozulu@gmail.com>
2026-02-24 10:48:07 +0100

    feat: Implement initial application structure with core pages, sections, shared components, and foundational styling.

---
+++ src/app/sections/product-showcase/product-showcase.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-showcase',
+  imports: [],
+  templateUrl: './product-showcase.html',
+  styleUrl: './product-showcase.scss',
+})
+export class ProductShowcase {
+
+}
---
+++ src/app/sections/value-propositions/value-propositions.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-value-propositions',
+  imports: [],
+  templateUrl: './value-propositions.html',
+  styleUrl: './value-propositions.scss',
+})
+export class ValuePropositions {
+
+}
