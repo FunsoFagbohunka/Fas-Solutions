7686289ee676304d175559bef20bc82824f30961
Umeozulu Anthony <tonyumeozulu@gmail.com>
2026-03-01 12:23:03 +0100

    Enhance navigation by updating header links to use routerLink for improved routing and enable anchor scrolling in app configuration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,7 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top', anchorScrolling: 'enabled' })),
     provideClientHydration(withEventReplay()),
   ],
 };
